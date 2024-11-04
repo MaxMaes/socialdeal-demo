671624eeba66dc6dee4a3ea7665841e9c91fe5f2
Max Maes <max@livewallgroup.com>
2024-11-04 20:22:03 +0100

    feat: Added Application class for DI

---
+++ app/src/main/java/nl/mythicproductions/socialdealdemo/SocialDealDemoApplication.kt
@@ -0,0 +1,12 @@
+package nl.mythicproductions.socialdealdemo
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SocialDealDemoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
