fb4dc059a2d01d11ffe42bb1c4753ebad3233b01
Max Maes <max@livewallgroup.com>
2024-11-05 09:51:50 +0100

    feat: Added splash screen
    Nothing special, just so it's there

--- app/src/main/java/nl/mythicproductions/socialdealdemo/MainActivity.kt
+++ app/src/main/java/nl/mythicproductions/socialdealdemo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import nl.mythicproductions.socialdealdemo.ui.navigation.RootNavigator
 import nl.mythicproductions.socialdealdemo.ui.theme.SocialDealDemoTheme
@@ -12,6 +13,8 @@ import nl.mythicproductions.socialdealdemo.ui.theme.SocialDealDemoTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         enableEdgeToEdge()
         setContent {
             SocialDealDemoTheme {
